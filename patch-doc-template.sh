0e20481e8f5b9e35d3ad324e1be713b519b1c986
Antonio <antoniorees74@gmail.com>
2026-02-02 10:02:05 +0000

    Fix documentation-template patch for Netlify build

--- patch-doc-template.sh
+++ patch-doc-template.sh
@@ -8,15 +8,12 @@ if [ -f "$PATCH_FILE" ]; then
   if ! grep -q "window.__require" "$PATCH_FILE"; then
     echo "Patching documentation-template for browser compatibility..."
     
-    # Add React imports after the react-router-dom import
-    sed -i '/import { Link, Route, Routes, useLocation, useNavigate, useParams } from "react-router-dom";/a import * as React from '\''react'\'';\nimport * as ReactDOM from '\''react-dom'\'';' "$PATCH_FILE"
-    
     # Add the __require shim after the __require definition
     sed -i '/throw Error("Calling `require` for "\\"" x "\\"" in an environment that doesn'\''t expose the `require` function.");/a\
 \
 \/\/ ESM shim for __require\
 window.__require = function(moduleName) {\
-  if (moduleName === '\''react'\'') return React;\
+  if (moduleName === '\''react'\'') return { createContext, createElement, forwardRef, useContext, useEffect, useMemo, useRef, useState };\
   throw new Error('\''Dynamic require not supported: '\'' + moduleName);\
 };' "$PATCH_FILE"
     
