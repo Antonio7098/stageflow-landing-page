4d60f827440c55be5b7372d2c8971b543ee548b1
Antonio <antoniorees74@gmail.com>
2026-02-02 10:13:51 +0000

    Add vite-plugin-static-copy to copy stageflow-docs to dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import commonjs from 'vite-plugin-commonjs'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   plugins: [
     commonjs(),
     react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'stageflow-docs/**/*',
+          dest: 'stageflow-docs'
+        }
+      ]
+    })
   ],
   optimizeDeps: {
     include: ['react', 'react-dom', 'react-router-dom', 'documentation-template'],
